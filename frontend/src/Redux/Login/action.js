37e1e26479e906524bbcd2e3edc94e889e0c8062
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-01-28 20:30:17 +0530

    login action

---
+++ frontend/src/Redux/Login/action.js
@@ -0,0 +1,20 @@
+import { LOGIN_ERROR, LOGIN_SUCCESS, LOGIN_REQUEST } from "./type";
+import axios from "axios";
+
+const loginAction = (credentials) => async (dispatch) => {
+  try {
+    dispatch({ type: LOGIN_REQUEST });
+
+    const { data } = await axios.post("/api/user/login", credentials);
+
+    if (data.error) {
+      dispatch({ type: LOGIN_ERROR, error: data.error });
+    }
+    if (data.token) {
+      dispatch({ type: LOGIN_SUCCESS });
+      localStorage.setItem("token", data.token);
+    }
+  } catch (error) {
+    dispatch({ type: LOGIN_ERROR, error: error.message });
+  }
+};
