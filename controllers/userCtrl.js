f0ace6e72d365effb1a1e0318e04365fbc0168fc
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-01-28 19:36:49 +0530

    login validation

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,6 +2,13 @@ const joi = require("joi");
 const bcrypt = require("bcrypt");
 const userSchema = require("../model/userModel");
 
+const loginSchema = joi.object({
+  email: joi
+    .string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
+  // password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).min(6).max(12),
+});
+
 const userCtrl = {
   register: async (req, res) => {
     try {
@@ -14,10 +21,29 @@ const userCtrl = {
         email: email,
         password: hashedPassword,
       });
-      user.save()
-      res.json({created: true})
+      user.save();
+      res.json({ created: true });
     } catch (error) {
-        res.json({error:error.message});
+      res.json({ error: error.message });
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const value = await loginSchema.validate({ email: email });
+      if (value.error) return res.json({ error: value.error.message  });
+
+      const User = await userSchema.findOne({ email: email });
+
+      if (!User) return res.json({ error: "User not found" });
+
+      const validDatePassword = await bcrypt.compare(password, User.password);
+
+      if (!validDatePassword) return res.json({ error: "Password incorrect" });
+
+      res.json({ user: User });
+    } catch (error) {
+      res.json({ error: error.message  });
     }
   },
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,5 +2,6 @@ const Router = require("express").Router();
 const userCtrl = require("../controllers/userCtrl");
 
 Router.route("/create").post(userCtrl.register);
+Router.route("/login").post(userCtrl.login)
 
 module.exports = Router;
