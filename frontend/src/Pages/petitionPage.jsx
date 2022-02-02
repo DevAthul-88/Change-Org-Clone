1e2728f20a13fe60a068b0b9588ab01d4bc55a9d
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-02-02 18:27:32 +0530

    fixes

--- frontend/src/Pages/petitionPage.jsx
+++ frontend/src/Pages/petitionPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { petitionById } from "../Redux/Post/action";
+import { petitionById } from "../Redux/Single/action";
 import Loader from "../Components/Loader";
 
 function PetitionPage({ id }) {
   const dispatch = useDispatch();
-  const { loading, data, error } = useSelector((state) => state.post);
+  const { loading, data, error } = useSelector((state) => state.details);
   useEffect(() => {
     dispatch(petitionById(id));
   }, []);
