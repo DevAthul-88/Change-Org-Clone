f16434b80918f32d154b8e379057603a8720b932
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-02-09 17:41:46 +0530

    make it better

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ mongoose.connect(process.env.MONGO_URL, (err) => {
   console.log("Connection established");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
